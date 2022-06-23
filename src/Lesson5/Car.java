55bd103e4782b3be2cf0c991da91126160106ddd
001 <d.akimov96@bk.ru>
2022-06-23 16:20:55 +0500

    ООП

--- src/Lesson5/Car.java
+++ src/Lesson5/Car.java
@@ -1,6 +1,11 @@
 package Lesson5;
 
 public class Car {
+    //fields
+    public String model;
+    public int year;
+    public boolean wasCrashed;
+
 
 
 }
---
+++ src/Lesson5/DemoApp.java
@@ -0,0 +1,10 @@
+package Lesson5;
+
+public class DemoApp {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println("Model =" + car.model + " year =" + car.year + " wasCrashed = " + car.wasCrashed);
+
+
+    }
+}
