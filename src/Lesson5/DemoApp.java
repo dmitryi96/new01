028f128137a1ce15780a14360ceb6d90dda33c33
001 <d.akimov96@bk.ru>
2022-07-13 16:44:34 +0500

    ООП2

--- src/Lesson5/DemoApp.java
+++ src/Lesson5/DemoApp.java
@@ -3,8 +3,22 @@ package Lesson5;
 public class DemoApp {
     public static void main(String[] args) {
         Car car = new Car();
-        System.out.println("Model =" + car.model + " year =" + car.year + " wasCrashed = " + car.wasCrashed);
+        car.model = "Dodge;";
+        car.year = 2020;
 
 
+
+        System.out.println("Model = " + car.model + " year = " + car.year + "; wasCrashed = " + car.wasCrashed+ ";");
+
+
+        Car car2 = new Car();
+        car2.model = "Ford;";
+        car2.year = 1990;
+
+
+
+        System.out.println("Model = " + car2.model + " year = " + car2.year + "; wasCrashed = " + car2.wasCrashed +";");
+
+        Car[] cars = new Car[]{car, car2};
     }
 }
